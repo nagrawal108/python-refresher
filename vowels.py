713de6cee8948be6ad819b85ae46ec2750acedfd
HQ\AgrawalN <nikhil.agrawal@rotary.org>
2026-03-26 14:30:01 -0500

    even numbers added

--- vowels.py
+++ vowels.py
@@ -15,3 +15,8 @@ def count_vowels(strings):
 input_strings = ["Hello World", "Python Programming", "OpenAI"]
 vowel_counts = count_vowels(input_strings)
 print(vowel_counts)  # Output: [3, 4, 4]
+
+
+#create a list of even number from 1 to 20
+even_numbers = [num for num in range(1, 21) if num % 2 == 0]
+print(even_numbers)  # Output: [2, 4, 6, 8, 10, 12, 14, 16, 18, 20] 
